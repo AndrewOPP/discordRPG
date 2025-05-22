e055c3fcddda13880783bbaa94a39e40c6731189
AndrewOPP <a.vyblov2003@gmail.com>
2025-05-22 17:28:28 +0300

    +clown +fixes
    # Conflicts:
    #	bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import asyncio
 import discord
+import os
 from discord import Message
 from src.config import settings
 from src.logs import getLogger
+from discord.ext import commands
 
 getLogger("discord.client").setLevel("WARNING")
 getLogger("discord.gateway").setLevel("WARNING")
@@ -10,25 +12,26 @@ getLogger("discord.gateway").setLevel("WARNING")
 
 logger = getLogger(__name__)
 
-intents = discord.Intents.default()
-intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
-
-@client.event
+@bot.event
 async def on_ready():
-    logger.info(f'START {client.user}')
+    logger.info(f'START {bot.user}')
 
 
-@client.event
+@bot.event
 async def on_message(message: Message):
     print(message.content)
 
+async def load_commands():
+    for filename in os.listdir("./src/cogs"):
+        if filename.endswith(".py") and filename != "__init__.py":
+            await bot.load_extension(f"src.cogs.{filename[:-3]}")
 
-def main():
-    client.run(settings.discord.token)
-
+async def main():
+    await load_commands()
+    await bot.start(settings.discord.token)
 
 if __name__ == "__main__":
     asyncio.run(main())
